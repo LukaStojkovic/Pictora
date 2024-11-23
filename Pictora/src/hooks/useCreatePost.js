b25efabbc4ca3b030f060232bc72020110700630
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2024-11-23 15:01:13 +0100

    Fixed Bug: Infinite request call

--- Pictora/src/hooks/useCreatePost.js
+++ Pictora/src/hooks/useCreatePost.js
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createPost as createPostApi } from "../services/apiPost";
 
 export function useCreatePost() {
+  const queryClient = useQueryClient();
+
   const { mutate: createPost, isLoading } = useMutation({
     mutationFn: (postData) => createPostApi(postData),
     onSuccess: () => {
       toast.success("You Created New Post!");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
     onError: (er) => {
       toast.error(er.message);
--- Pictora/src/hooks/useGetFeedPosts.js
+++ Pictora/src/hooks/useGetFeedPosts.js
@@ -2,13 +2,10 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getFeedPosts } from "../services/apiPost";
 
 export function useGetFeedPosts() {
-  const queryClient = useQueryClient();
-
   const { data: posts, isLoading } = useQuery({
     queryFn: () => getFeedPosts(),
     queryKey: ["posts"],
   });
-  queryClient.invalidateQueries({ queryKey: ["posts"] });
 
   return { posts, isLoading };
 }
