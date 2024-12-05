c2b255d464c63175e07a59d7be34264c9c63bf19
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2024-12-05 18:24:31 +0100

    Fixed picture bug

--- Pictora/src/features/posts/Comment.jsx
+++ Pictora/src/features/posts/Comment.jsx
@@ -7,7 +7,7 @@ function Comment({ comment }) {
       <Link to={`/profile/${comment.userId}`}>
         <img
           className="rounded-full h-5 w-5 mr-2 items-center"
-          src={`${import.meta.env.VITE_API_URL}/public/assets/${
+          src={`${import.meta.env.VITE_API_URL}/assets/${
             comment.userPicturePath
           }`}
         />
